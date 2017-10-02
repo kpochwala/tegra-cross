24308049c660db061d82ee9b75567d6b6fd1552d
kpochwala <krzysztof.pochwala@vrhalla.com>
2017-10-03 00:51:50 +0200

    added script downloading toolchain

---
+++ summon-linaro-toolchain.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://releases.linaro.org/components/toolchain/binaries/latest-5/aarch64-linux-gnu/gcc-linaro-5.4.1-2017.05-x86_64_aarch64-linux-gnu.tar.xz
+
+tar -xvf gcc-linaro-5.4.1-2017.05-x86_64_aarch64-linux-gnu.tar.xz
